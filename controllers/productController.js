9020053a2fcce534d2954cb645c1e11bc01e24d3
GeronimoTiscornia <gero.tisco@gmail.com>
2024-11-19 15:17:15 -0300

    empezando producto

---
+++ controllers/productController.js
@@ -0,0 +1,16 @@
+//Importar base de datos
+//Importar operadores
+
+let productController = {
+    list: (req, res) => {
+        return res.render("Lista")
+    },
+
+    detalle: (req, res) => {
+        return res.render("Detalle")
+    },
+
+    list: (req, res) => {
+        return res.render("Lista")
+    }
+}
--- routes/products.js
+++ routes/products.js
@@ -8,4 +8,8 @@ router.get('/', function(req, res) {
     return  res.send('TODOS LOS PRODUCTOS')
 })
 
+//router.get('/', productController.list)
+
+//router.get('/:id', productController.detalle)
+
 module.exports = router;
