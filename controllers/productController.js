b0c28e4a17eabc09b34fa5628d7380b66c9f98f3
GeronimoTiscornia <gero.tisco@gmail.com>
2024-11-21 10:20:01 -0300

    controller y rutas de producto

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,11 +4,11 @@ const db = require('../database/models/Product');
 const Product = db.Product;
 
 const productController = {
-    list: (req, res) => {
+    index: (req, res) => {
         db.Product.findAll()
-        .then(function (result) {
-            return res.send(result)
-        })
+            .then(function (result) {
+                return res.send(result)
+            })
     },
     mostrarProducto: (req, res) => {
         if (!req.session || !req.session.user) {
@@ -21,20 +21,48 @@ const productController = {
         if (!req.session || !req.session.user) {
             return res.redirect('/login');
         }
-        const {imagen, nombre, descripcion} = req.body;
+        const { imagen, nombre, descripcion } = req.body;
     },
 
     detalle: (req, res) => {
         let id = req.params.id
 
         db.Product.findByPk(id)
-        .then(function(result) {
-            return res.send(result)
-        })
-        .catch(function(err) {
-            return console.log(err);
-        })
+            .then(function (result) {
+                return res.send(result)
+            })
+            .catch(function (err) {
+                return console.log(err);
+            })
     },
+
+    nueva: (req, res) => {
+        let filtro1 = {
+            order: [['release_date', 'DESC']],
+        }
+
+        db.Product.findAll(filtro1)
+            .then(function (result) {
+                return res.send(result)
+            })
+            .catch(function (err) {
+                return console.log(err);
+            })
+    },
+
+    search: (req, res) => {
+        // let qs = req.query.//falta saber el nombre
+        let filtro = {
+            where: [{ title: { [op.like]: `%${qs}%` } }]
+        }
+        db.Product.findOne(filtro)
+            .then((result) => {
+                return res.send(result)
+            })
+            .catch((err) => {
+                return console.log(err);
+            })
+    }
 }
 
 module.exports = productController;
--- routes/products.js
+++ routes/products.js
@@ -5,12 +5,16 @@ var router = express.Router();
 let productController = require('../controllers/productController');
 
 //RUTAS PRODUCTS
-router.get('/', productController.list)
+router.get('/', productController.index)
 
 router.get('/', productController.mostrarProducto);
 
 router.post('/', productController.procesarProducto);
 
-router.get('/:id', productController.detalle)
+router.get('/detail/:id', productController.detalle);
+
+router.get('/new', productController.nueva);
+
+router.get('/search', productController.search);
 
 module.exports = router;
