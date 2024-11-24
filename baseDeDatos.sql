c7c96bea36b4a988c7c7bb299ffa480c77be09c8
lucashabib <lhabib@udesa.edu.ar>
2024-11-24 20:32:04 -0300

    Base de Datos y Asociacion completa

--- baseDeDatos.sql
+++ baseDeDatos.sql
@@ -2,6 +2,8 @@ CREATE SCHEMA maquina_retro;
 
 use maquina_retro;
 
+-- TABLA USERS
+
 create table users (
 	id 				INT		UNSIGNED		PRIMARY KEY		AUTO_INCREMENT,
     nombre			VARCHAR(100)	NOT NULL,
@@ -21,5 +23,26 @@ INSERT INTO users (nombre, email, contrasena) VALUES
 ('Juan', 'juan@gmail.com', 'hashed4_ejemplo'),
 ('Carlos', 'carlos@gmail.com', 'hashed5_ejemplo');
 
+-- TABLA PRODUCTS
 
--- FALTA CREAR LA TABLA DE PRODUCTS Y LE AGREGAN LOS VALORES EN database/models CREARLE 5 VALORES POR DEFAULT (ES CONSIGNA DEL TP QUE YA TENGA 5 VALORES)
+CREATE TABLE products (
+	productId 				INT		UNSIGNED		PRIMARY KEY		AUTO_INCREMENT,
+    productNombre			VARCHAR(100)	NOT NULL,
+    productPrecio			VARCHAR(100)	NOT NULL,
+    productDescripcion		VARCHAR(300)	NOT NULL,
+    userId                  INT     UNSIGNED    NOT NULL,
+    
+    FOREIGN KEY (userId) REFERENCES users(id),
+    
+    -- COLUMNAS DE AUDITORIA
+    createdAt	TIMESTAMP	DEFAULT	CURRENT_TIMESTAMP,
+    updatedAt	TIMESTAMP	DEFAULT	CURRENT_TIMESTAMP,
+    deletedAt	TIMESTAMP	DEFAULT	CURRENT_TIMESTAMP
+);
+
+INSERT INTO products (productNombre, productPrecio, productDescripcion, userId) VALUES
+('River Plate Local 1986', '$60.000', 'Remera Local del Club Atletico River Plate del año 1986', 2),
+('Boca Juniors Local 2003', '$60.000', 'Remera Local del Club Atletico Boca Juniors del año 2003', 1),
+('Barcelona Visitante 2011', '$55.000', 'Remera Visitante del Fútbol Club Barcelona del año 2011', 3),
+('Bayern Munich Alternativa 1998', '$70.000', 'Remera Alternativa del Fußball-Club Bayern München del año 1998', 2),
+('Real Madrid Local 2000', '$65.000', 'Remera Local del Real Madrid Club de Fútbol del año 2000', 5);
