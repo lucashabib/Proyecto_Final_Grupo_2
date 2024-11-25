48a85b5d6deb8ad4d178a80a788b12c622312541
GeronimoTiscornia <gero.tisco@gmail.com>
2024-11-25 17:17:44 -0300

    search

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,16 +3,18 @@ const db = require('../database/models');
 //Importar operadores
 const Product = db.Product;
 
+const Op = db.Sequelize.Op
+
 const productController = {
 
     show: function (req, res) {
-        return res.render('product-add')
+        return res.render('index')
     },
 
 
     mostrarProducto: (req, res) => {
         if (!req.session || !req.session.user) {
-            return res.redirect('/user/login');
+            return res.redirect('/users/login');
         }
         res.render('product-add');
     },
@@ -21,7 +23,7 @@ const productController = {
         if (req.session.user != undefined) {
             return res.render("product-add", { title: "Agregar Producto" })
         }
-        return res.redirect("/user/login")  //para el boton de agregar producto, si esta registrado o no
+        return res.redirect("/users/login")  //para el boton de agregar producto, si esta registrado o no
     },
 
     procesarProducto: (req, res) => {
@@ -52,20 +54,21 @@ const productController = {
     },
 
     search: (req, res) => {
-        let qs = req.query.Product;
-
+        let qs = req.query.search;
+        
         let filtro = {
-            where: [{ title: { [op.like]: `%${qs}%` } }],
+            where: [{ title: { [Op.like]: `%${qs}%` } }],
             order: [[ 'createAt', 'DESC']], 
-            include: [{ model: db.User, as: 'user' }]
+            include: [{ model: db.User, as: 'User' }]
         };
 
         db.Product.findAll(filtro)
             .then(function (results) {
+                return res.send(results)
                 if (results.length > 0) {
                     return res.render('search-results', { results: results })
                 } else {
-                    return res.send('No hay resultados para su criterio de busqueda')
+                    return res.send('No haay resultados para su criterio de busqueda')
                 }
             })
             .catch(function (err) {
@@ -80,9 +83,9 @@ const productController = {
 
             .then(function (result) {
                 if (result) {
-                    return res.render('product', { product: product })
+                    return res.render('product', { product: result })
                 } else {
-                    return res.render('Producto no encontrado');
+                    return res.send('Producto no encontrado');
                 }
             })
             .catch(function (err) {
